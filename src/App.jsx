a78481e5248a8ba62412c3d312fe240c88db0932
trisssh <samillanotrisha768@gmail.com>
2026-09-03 13:50:45 +0800

    Uploaded assets and Added Program Flow Section

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,11 @@ import "./App.css";
 
 function App() {
   ////////////////////YYYY-MM-DD-HH-MM-SS
-  // const targetDate = "2026-09-25T09:00:00";
-  const targetDate = "2026-09-03T17:00:00";
+  const targetDate = "2026-09-25T09:00:00";
+  // const targetDate = "2026-09-03T17:00:00";
 
   return (
     <>
-      {/* TOP IMAGE SECTION */}
-      {/* <section id="home" className="w-full">
-        <img
-          src="BANNER.png"
-          alt="Top banner"
-          className="w-full h-auto object-contain"
-        />
-      </section> */}
-
       {/* HERO - BANNER */}
       {/* <section className="relative min-h-screen overflow-hidden bg-gradient-to-b from-neutral-950 via-black to-neutral-900"> */}
       <section className="relative h-[60vh] md:h-[82vh] overflow-hidden bg-gradient-to-b from-neutral-950 via-black to-neutral-900">
@@ -79,25 +70,29 @@ function App() {
         </div>
       </section>
 
-      {/* ABOUT - DESCRIPTION SECTION */}
-      <section className="grid min-h-screen items-center justify-center grid-cols-1">
-        <div className="grid justify-center">
-          <div className="grid justify-center mb-4">
-            <span className="inline-block px-5 py-1 rounded-full text-base text-gray-700 transition bg-white shadow-md border border-gray-100 font-mono font-semibold">
-              September 2026
-            </span>
-          </div>
+      {/* MAIN CONTENT */}
+      <main>
+        {/* ABOUT - DESCRIPTION SECTION */}
+        <section className="flex flex-col min-h-svh md:min-h-dyh items-center justify-center md:pt-6">
+          <div>
+            {/* ROUND BTN STYLE DATE */}
+            <div className="grid justify-center mb-4">
+              <span className="inline-block px-5 py-1 rounded-full text-base text-gray-700 transition bg-white shadow-md border border-gray-100 font-mono font-semibold">
+                September 2026
+              </span>
+            </div>
 
-          <div className="mb-8">
-            <h3 className="font-medium text-2xl md:text-4xl text-center">
-              Lorem ipsum dolor sit amet
-            </h3>
-            <h5 className="text-sm md:text-xl text-center">
-              Lorem ipsum dolor sit amet consectetur
-            </h5>
-          </div>
+            {/* TITLE & TEXT */}
+            <div className="mb-8">
+              <h3 className="font-medium text-2xl md:text-4xl text-center">
+                Lorem ipsum dolor sit amet
+              </h3>
+              <h5 className="text-sm md:text-xl text-center">
+                Lorem ipsum dolor sit amet consectetur
+              </h5>
+            </div>
 
-          <p className="text-justify text-md max-w-md px-8 md:px-0">
+            {/* <p className="text-justify text-md max-w-md px-8 md:px-0">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -105,84 +100,131 @@ function App() {
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
+          </p> */}
 
-        {/* Circle Logo */}
-        <div className="flex flex-row items-center justify-center py-4">
-          <span>
-            <hr className="w-30 md:w-50 lg:w-110 text-[#D2AA0D]  filter drop-shadow-[0_0_0.25rem_#D2AA0D]"></hr>
-          </span>
-          <img
-            src="spclogo.png"
-            className="bg-white p-0.5 mx-1 filter  border-2 drop-shadow-[0_0_0.25rem_#D2AA0D]  border-yellow-400 rounded-full w-20 h-20 zoom"
-          />
-          <span>
-            <hr className="w-30 md:w-50 lg:w-110 text-[#D2AA0D] filter drop-shadow-[0_0_0.25rem_#D2AA0D]"></hr>
-          </span>
-        </div>
-      </section>
+            {/* BANNER FROM CSC */}
+            <div className="flex justify-center">
+              <img
+                src="gawad-sample-banner.png"
+                className="w-[315px] md:w-[500px] backdrop-blur-lg bg-white border border-gray-200 rounded-2xl shadow-md md:col-span-1 object-fill"
+              />
+            </div>
+          </div>
 
-      {/* CARD - LOYALTY AWARDEES */}
-      <section className="bg-amber-100 p-6 mb-18 rounded-lg shadow-lg mx-10 md:mx-20">
-        <div className="flex flex-col items-center space-x-4">
-          <div className="text-center mb-6">
-            <h1 className="text-2xl md:text-3xl font-bold mb-2 text-gray-800 text-center">
-              LOYALTY AWARDEES 2026
-            </h1>
-            <span className="text-gray-600 text-center text-sm">
-              Recognizing Decades of Service Excellence
+          {/* Circle Logo */}
+          <div className="flex flex-row items-center justify-center py-6">
+            <span>
+              <hr className="w-30 md:w-50 lg:w-110 text-[#D2AA0D]  filter drop-shadow-[0_0_0.25rem_#D2AA0D]"></hr>
+            </span>
+            <img
+              src="spclogo.png"
+              className="bg-white p-0.5 mx-1 filter  border-2 drop-shadow-[0_0_0.25rem_#D2AA0D]  border-yellow-400 rounded-full w-20 h-20 zoom"
+            />
+            <span>
+              <hr className="w-30 md:w-50 lg:w-110 text-[#D2AA0D] filter drop-shadow-[0_0_0.25rem_#D2AA0D]"></hr>
             </span>
           </div>
+        </section>
 
-          {/* Years logo + Num of Awardee */}
-          <div className="grid grid-cols-2 md:flex gap-6">
-            <div className="flex flex-col items-center justify-evenly px-6 py-3">
+        {/* Program Flow */}
+        <section className="lg:min-h-screen bg-amber-50 py-6  justify-center items-center ">
+          {/* <h2 className="text-center uppercase font-mono text-2xl px-3 md:text-3xl font-semibold pt-8 pb-6">
+            San Pablo City of Lakes Festival
+          </h2> */}
+
+          <div className=" flex flex-col-reverse md:grid md:grid-cols-3 gap-4 p-3 max-w-6xl mx-auto bg-re">
+            {/* LEFT SIDE */}
+            <div className="flex justify-center">
               <img
-                src="25-logo.png"
-                className="filter drop-shadow-[0_0_0.25rem_#2b2b2b] rounded-full w-25 md:w-40"
+                src="prog-flow.png"
+                className="w-[380px] md:w-full backdrop-blur-lg bg-white border border-gray-200 rounded-2xl shadow-md md:col-span-1 object-fill"
               />
-
-              <span className="text-gray-600 text-center capitalize text-xs md:text-lg mt-2">
-                42 Employees awardee
-              </span>
             </div>
-            <div className="flex flex-col items-center justify-evenly px-6 py-3">
-              <img
-                src="30-logo.png"
-                className="filter drop-shadow-[0_0_0.25rem_#2b2b2b] rounded-full w-25 md:w-40"
-              />
 
-              <span className="text-gray-600 text-center capitalize text-xs md:text-lg mt-2">
-                13 Employees awardee
-              </span>
-            </div>
-            <div className="flex flex-col items-center justify-evenly px-6 py-3">
-              <img
-                src="35-logo.png"
-                className="filter drop-shadow-[0_0_0.25rem_#2b2b2b] rounded-full w-25 md:w-40"
-              />
+            {/* RIGHT SIDE */}
+            <div className="flex flex-col md:col-span-2 justify-center">
+              <div className="flex justify-center">
+                <img
+                  src="G A W A D - 2026.png"
+                  // src="126th PCSA logo (alternate 1).png"
+                  className="w-50 md:w-75 lg:w-120 "
+                />
+              </div>
 
-              <span className="text-gray-600 text-center capitalize text-xs md:text-lg mt-2">
-                4 Employees awardee
-              </span>
-            </div>
-            <div className="flex flex-col items-center justify-evenly px-6 py-3">
-              <img
-                src="40-logo.png"
-                className="filter drop-shadow-[0_0_0.25rem_#2b2b2b] rounded-full w-25 md:w-40"
-              />
+              <h2 className="text-center uppercase font-mono text-xl px-3  md:text-3xl font-semibold py-2">
+                PROGRAM FLOW OF <br /> GAWAD PARANGAL 2026
+              </h2>
 
-              <span className="text-gray-600 text-center capitalize text-xs md:text-lg mt-2">
-                4 Employees awardee
-              </span>
+              {/* <p className="text-xs md:text-base text-center md:px-20">
+                Lorem ipsum is placeholder text commonly used in the graphic,
+                print, and publishing industries for previewing layouts and
+                visual mockups.
+              </p> */}
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+
+        {/* CARD - LOYALTY AWARDEES */}
+        <section className="bg-amber-100 p-6 rounded-lg shadow-lg m-12 md:m-20">
+          <div className="flex flex-col items-center space-x-4">
+            <div className="text-center mb-6">
+              <h1 className="text-2xl md:text-3xl font-bold mb-2 text-gray-800 text-center">
+                LOYALTY AWARDEES 2026
+              </h1>
+              <span className="text-gray-600 text-center text-sm">
+                Recognizing Decades of Service Excellence
+              </span>
+            </div>
+
+            {/* Years logo + Num of Awardee */}
+            <div className="grid grid-cols-2 md:flex gap-6">
+              <div className="flex flex-col items-center justify-evenly px-6 py-3">
+                <img
+                  src="25-logo.png"
+                  className="filter drop-shadow-[0_0_0.25rem_#2b2b2b] rounded-full w-25 md:w-40"
+                />
+
+                <span className="text-gray-600 text-center capitalize text-xs md:text-lg mt-2">
+                  42 Employees awardee
+                </span>
+              </div>
+              <div className="flex flex-col items-center justify-evenly px-6 py-3">
+                <img
+                  src="30-logo.png"
+                  className="filter drop-shadow-[0_0_0.25rem_#2b2b2b] rounded-full w-25 md:w-40"
+                />
+
+                <span className="text-gray-600 text-center capitalize text-xs md:text-lg mt-2">
+                  13 Employees awardee
+                </span>
+              </div>
+              <div className="flex flex-col items-center justify-evenly px-6 py-3">
+                <img
+                  src="35-logo.png"
+                  className="filter drop-shadow-[0_0_0.25rem_#2b2b2b] rounded-full w-25 md:w-40"
+                />
+
+                <span className="text-gray-600 text-center capitalize text-xs md:text-lg mt-2">
+                  4 Employees awardee
+                </span>
+              </div>
+              <div className="flex flex-col items-center justify-evenly px-6 py-3">
+                <img
+                  src="40-logo.png"
+                  className="filter drop-shadow-[0_0_0.25rem_#2b2b2b] rounded-full w-25 md:w-40"
+                />
+
+                <span className="text-gray-600 text-center capitalize text-xs md:text-lg mt-2">
+                  4 Employees awardee
+                </span>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
 
       {/* FOOTER */}
-      <footer className="bg-gradient-to-b from-black to-slate-800 text-amber-500">
+      <footer className="bg-gradient-to-b from-black to-slate-800 text-white">
         <div className="max-w-5xl mx-auto flex flex-row items-center justify-between gap-3 px-6 py-4">
           <img
             src="spclogo.png"
@@ -192,7 +234,10 @@ function App() {
 
           <p className="text-center text-xs md:text-sm font-semibold leading-snug">
             © 2026{" "}
-            <a href="#" className="hover:underline hover:text-amber-400">
+            <a
+              href="https://www.facebook.com/spc.mis"
+              className="hover:underline hover:text-amber-400"
+            >
               Management Information System Office.
             </a>
             <br />
